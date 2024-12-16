c65f52fe8bae742a4aa6becad5d46c294711de3e
Mukund2074 <166690451+Mukund2074@users.noreply.github.com>
2024-12-16 16:24:06 +0530

    Update page.jsx

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -7,7 +7,6 @@ export default function Home() {
 
   const { mainVideos , extraVideos , shorts , loadingInitial , loadingMore , error  , setPath , setContinuationPath, setEndRef } = useDataContext();
   const endRef = useRef(null);
-  // setEndRef(endRef);
   
 useEffect(() => {
   setEndRef(endRef);
